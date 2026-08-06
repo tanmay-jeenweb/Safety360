0e97961aaa242e470583324386e6084f6781332a
Tanmay <tanmay.jeenweb@gmail.com>
2026-08-06 16:05:26 +0530

    time change

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,6 +7,7 @@ const db = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    timezone: '+05:30',
 
     waitForConnections: true,
     connectionLimit: 10,
--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+process.env.TZ = 'Asia/Kolkata';
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
